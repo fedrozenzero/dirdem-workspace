7a65df550e96ffcc10b104391cac6cb3813b0bd1
adm91 <adm91@email.com>
2021-03-03 20:20:48 +0100

    good

--- apps/dirdem-serial/src/app/app.module.ts
+++ apps/dirdem-serial/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { FuseBitModule } from 'libraries/fuse-bit/public-api';
 import { TechnicalSpecificationsModule } from 'libraries/technical-specifications/lib/technical-specifications.module';
 import { MaterialModule } from 'libraries/material/material.module';
 import { TerminalModule } from 'libraries/terminal/public-api';
-import { SerialMonitorModule } from 'libraries/serial-monitor/src/public-api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SerialMonitorModule } from 'libraries/serial-monitor/public-api';
 
 @NgModule({
   declarations: [
